6f18fc4588656c0c152e2963b16809e770eaafb1
aimansae <saeedaimann@gmail.com>
2022-06-27 21:23:40 +0000

    Add form onsubmit function to store  player's username

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,10 @@
-alert('check');
+function sendForm (event) {
+    event.preventDefault();
+    let quizForm = document.forms['quiz-form']['username'].value;
+    console.log('SEND');
+
+    // code to store player's name
+
+    sessionStorage.setItem('username', quizForm);
+    console.log(quizForm);
+}
